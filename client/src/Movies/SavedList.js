6ff860d9665727a0ae1c46862fcb4a295b177d15
zee <zzdd9251@gmail.com>
2019-09-18 14:17:13 -0700

    project done final..

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,12 +1,31 @@
-import React from 'react';
+
+
+
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { Button } from "reactstrap";
+import styled from "styled-components";
+
+const SavedMovies = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
 
 const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
-    {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
-    ))}
-    <div className="home-button">Home</div>
+    <SavedMovies>
+      {props.list.map(movie => (
+        <NavLink key={movie.id} to={`/movies/${movie.id}`}>
+          <span className="saved-movie">{movie.title}</span>
+        </NavLink>
+      ))}
+    </SavedMovies>
+    <Link to="/">
+      <Button outline color="primary" size="lg">
+        Home
+      </Button>
+    </Link>
   </div>
 );
 
